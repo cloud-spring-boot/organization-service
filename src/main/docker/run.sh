61cf0bb79662eae327d56ee9a84d581270238a7f
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-10-20 17:33:03 +0300

    tcpdump added

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 echo "********************************************************"
 echo "Waiting for the Eureka to start on port $EUREKASERVER_PORT"
 echo "********************************************************"
 while ! `nc -z eureka $EUREKASERVER_PORT `; do sleep 3; done
 echo ">>>>>>>>>>>> Eureka has started"
+
+nohup tcpdump -A -s 0 'tcp port 8080 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)' \
+-w /usr/local/organization-service/pcap/organization.pcap &
 
 echo "********************************************************"
 echo "Starting organization-service "
--- src/main/java/com/max/organization/OrganizationApplication.java
+++ src/main/java/com/max/organization/OrganizationApplication.java
@@ -7,10 +7,12 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@EnableResourceServer
 @EnableEurekaClient
+@SpringBootApplication
 public class OrganizationApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrganizationApplication.class, args);
