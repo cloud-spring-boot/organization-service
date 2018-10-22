f51401ee6364d6c5b16eadfc744b3dee7ea07d3e
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-10-22 17:44:43 +0300

    small changes

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -6,7 +6,7 @@ while ! `nc -z eureka $EUREKASERVER_PORT `; do sleep 3; done
 echo ">>>>>>>>>>>> Eureka has started"
 
 nohup tcpdump -A -s 0 'tcp port 8080 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)' \
--w /usr/local/organization-service/pcap/organization.pcap &
+-w /usr/local/organization-service/pcap/organization-$RANDOM.pcap &
 
 echo "********************************************************"
 echo "Starting organization-service "
