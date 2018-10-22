f3b9385f8cad3cefbab05a48a862ac4f101f3085
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-10-22 17:21:34 +0300

    declarative roles added

--- src/main/java/com/max/organization/controllers/OrganizationController.java
+++ src/main/java/com/max/organization/controllers/OrganizationController.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
 
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RestController
 @RequestMapping(value = "v1/organizations/")
 public class OrganizationController {
